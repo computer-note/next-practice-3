c2a653643013f0c6a4c4be92e5ea9f5bfacd37c7
computer-note <planomenes@gmail.com>
2024-07-21 20:19:35 +0900

    update (_components/TodoItem): 유저ID 정보를 통해 유저닉네임 정보를 Supabase DB로부터 패치
    컴포넌트 디자인 문제
      Presentational Component는 상위 컴포넌트로부터 모든 데이터를 받는다.
      Presentational Component는 이외의 데이터패치나 데이터가공 작업을 하지 않는다.
    
    DB리퀘스트 최적화 문제
      동일한 유저ID에 대해서 여러 번 유저닉네임 정보를 리퀘스트하고 있습니다.
      상위 컴포넌트에서 유저ID를 Set으로 모아서 각 유저ID에 대한 유저닉네임 Map을 생성합니다.

--- src/app/_components/TodoItem.tsx
+++ src/app/_components/TodoItem.tsx
@@ -1,11 +1,26 @@
-function TodoItem(props: any) {
+import { createClient } from '@/utils/supabase/server';
+
+async function TodoItem(props: any) {
   const { todo } = props;
+
+  const supabase = createClient();
+  const response = await supabase
+    .from('profiles')
+    .select('username')
+    .eq('id', todo.user_id);
+
+  const creater = response.data
+    ? response.data[0].username
+    : '익명의 사용자';
 
   //<article {...className['todo-card']}>
   return (
     <article className='w-[300px] border-solid border-[1px] bg-slate-100 p-[10px]'>
-      <p>
-        {todo.id}: {todo.created_at}
+      <p className='flex flex-col'>
+        <span>
+          {todo.id}. {todo.created_at}
+        </span>
+        <span>By {creater}</span>
       </p>
       <p>{todo.title}</p>
     </article>
