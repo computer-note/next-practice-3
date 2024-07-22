ff526e1bbe07a5d18491c674b501892c08340dec
computer-note <planomenes@gmail.com>
2024-07-22 17:37:18 +0900

    add (app/layout): 루트 레이아웃에서 인증정보를 패치해서 클라이언트 컴포넌트들에게 컨텍스트로 제공
    after
    클라이언트 컴포넌트들은 루트에서 실행된 supabase.auth.getUser 결과값을 사용할 수 있게 되었습니다.
    
    before
    클라이언트 컴포넌트들은 매번 각각 내부에서 supabase.auth.getUser를 실행해야 합니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,30 @@
-import Header from '@/components/Header';
 import './globals.css';
+import AuthProvider from '@/providers/AuthProvider';
+import Header from '@/components/Header';
+import { createClient } from '@/utils/supabase/server';
+import { type PropsWithChildren } from 'react';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout(props: PropsWithChildren) {
+  const { children } = props;
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  const authInfo = {};
+  if (error) {
+    authInfo.isLogin = false;
+  } else {
+    authInfo.isLogin = true;
+    authInfo.data = data.user;
+  }
+
   return (
     <html lang='en'>
       <body>
-        <Header />
-        {children}
+        <AuthProvider value={authInfo}>
+          <Header />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
