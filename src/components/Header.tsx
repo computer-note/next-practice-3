c2d2ed22c3b1119954d1ebf347a9fd2176c9b4ac
computer-note <planomenes@gmail.com>
2024-07-22 18:02:29 +0900

    update(Header): 공용 헤더에 로그인된 유저 프로필 정보를 표시

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useAuthContext } from '@/providers/AuthProvider';
 
 const pathMap = new Map([
   ['/', '메인페이지'],
@@ -11,37 +12,39 @@ const pathMap = new Map([
 ]);
 
 function Header() {
-  const currentUrl = usePathname();
+  const curPath = usePathname();
+  const { isLogin, user } = useAuthContext();
 
-  const currentPageName =
-    pathMap.get(currentUrl) || '알 수 없는 페이지입니다.';
+  const curPageName =
+    pathMap.get(curPath) || '알 수 없는 페이지입니다.';
 
-  const linkList = Array.from(pathMap.entries()).map(
-    ([relUrl, pageName]) => {
-      if (pageName === currentPageName) {
-        return null;
-      }
-
-      return <Link href={relUrl}>{pageName}로</Link>;
-    }
+  const filteredPathList = Array.from(pathMap.entries()).filter(
+    ([path]) => path !== curPath
   );
 
   const spanWidth = `${(1 / pathMap.size) * 100}%`;
   const navGroupWidth = `${(1 - 1 / pathMap.size) * 100}%`;
 
   return (
-    <header className='flex bg-slate-100 justify-between'>
+    <header className='flex bg-slate-200 justify-between'>
+      <div className='flex flex-col'>
+        <span>로그인 상태: {`${isLogin}`}</span>
+        <span>로그인한 유저: {user && user.username}</span>
+      </div>
+
       <span className={`w-[${spanWidth}]`}>
-        현재 페이지: {currentPageName}
+        현재 페이지: {curPageName}
       </span>
 
       <nav
         className={`w-[${navGroupWidth}] 
-        flex bg-slate-100 justify-between
-        gap-5
-        `}
+        flex justify-between gap-5`}
       >
-        {linkList}
+        {filteredPathList.map(([path, pageName]) => (
+          <Link key={path} href={path}>
+            {pageName}로
+          </Link>
+        ))}
       </nav>
     </header>
   );
